b0dae02f7419743efc255e96a7229a1996b8e347
ore gilron <ore gilron>
2016-09-13 13:35:56 +0300

    last version

--- js/imdb.js
+++ js/imdb.js
@@ -6,7 +6,7 @@ function handle(e){
   
   //tirgers the "enter" event
   if(e.keyCode === 13){
-  	
+    
     // Ensure it is only this code that run
     e.preventDefault(); 
     
@@ -37,43 +37,46 @@ function addMovie (e) {
 
   //create new "li" item in order to insert it to the existing "ul"
   var newItem = document.createElement("li");
-
-	 
+  var name = movieName.value;
+  var janner = movieJanner.value;
+  var time = movieTime.value;
+  var create = document.createTextNode;
+   
   //"create" text node to assign in the new "li" 
-  var newName = document.createTextNode(movieName.value + ", " + movieJanner.value + ", " + movieTime.value + " ");
+  var newName = create(name + ", " + janner + ", " + time + " ");
   //assinging the text node to the new "li"
   newItem.appendChild(newName);
 
   //adding the button option to the "li"
   newItem.appendChild(delButton);
-  
+
   //push the "li" to the state array
   liArray.push(newItem);
   
   //call showList with the text in the null as a value
   showList(null);
- 
-  
 };
 
+$( '.name' );
 
 //this function purpose is to prevent sumbitting 
 //unfull movie data.
 function disCheck() {
-	
-	// assign to movie@ the relevant text element
-	var movieName = document.getElementById("movieName");
-	var movieJanner = document.getElementById("movieJanner");
-	var movieTime = document.getElementById("movieTime");
-	
-	//condition for checking false insertion of "li"'s
-	if ((movieName.value !== "") && (movieJanner.value !== "") && (movieTime.value !== "")) {
+  
+  // assign to movie@ the relevant text element
+  var movieName = document.getElementById("movieName").value;
+  var movieJanner = document.getElementById("movieJanner").value;
+  var movieTime = document.getElementById("movieTime").value;
 
-		document.getElementById("buttn").disabled = false;
-	
-	}else {
-		document.getElementById("buttn").disabled = true;
-	}
+  
+  //condition for checking false insertion of "li"'s
+  if ((movieName !== "") && (movieJanner !== "") && (movieTime !== "")) {
+
+    document.getElementById("buttn").disabled = false;
+  
+  }else {
+    document.getElementById("buttn").disabled = true;
+  }
 
 };
 
@@ -86,32 +89,32 @@ function showList (e) {
   //will present all the elements in the array- all the list.
   if (e == null){
  
-   	var currentLi = document.getElementById("anchor"); 
-  	var ul = document.getElementById("mList");
+    var currentLi = document.getElementById("anchor"); 
+    var ul = document.getElementById("mList");
 
-  	ul.insertBefore(liArray[liArray.length -1 ], currentLi);
+    ul.insertBefore(liArray[liArray.length -1 ], currentLi);
  
   //in case e !== null (probably when calling the function 
   //from serach box and the serch box is not empty). 
   //will present all the elements in the array that 
   //the string in the search box is a substring of them.
-  }else {
+  } else {
      
     var ul = document.getElementById("mList");
-
+    
     //empty the "ul"
     ul.innerHTML = '';
-    
-    for (var i = 0; i < liArray.length; i++) {
-   
-      if (liArray[i].innerHTML.indexOf(e) !== -1){
-   	   
-   	  var currentLi = document.getElementById("anchor"); 
-  	  var ul = document.getElementById("mList");
-  	  ul.insertBefore(liArray[i], currentLi);
-  	   
-      }
+    liArray.filter(x).forEach(y);
+
+    function x (li){
+      return (li.innerHTML.indexOf(e) !== -1);
     }
+
+    function y (li) { 
+      var currentLi = document.getElementById("anchor"); 
+      var ul = document.getElementById("mList");
+      ul.insertBefore(li, currentLi);
+    }   
   }
 };
 
@@ -120,15 +123,13 @@ function showList (e) {
 function deleteLi (){
 
   //assingning x the "li" parent element of the button
-	var liParent = this.parentNode;
+  var liParent = this.parentNode;
 
   //constructing assistance array for manipulating 
   //the existing array and delete from the original array
   //the "li" whom the "X" button of his was submitted
-  var tempArray = liArray.filter(function(num,i){ 
-      return num !== liParent});
-  liArray = tempArray;
-	
+  liArray.splice(liArray.indexOf(this.parentNode),1);
+  
   //removing the "li"
-  this.parentNode.remove();
+    this.parentNode.remove();
 };
